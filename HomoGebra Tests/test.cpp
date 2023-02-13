655a8f9731d3bfe4ce3d41c2b58e3913b2c45c14
nook0110 <nik.blokhtin@gmail.com>
2023-02-13 15:47:49 +0300

    Coordinate.h: MatrixTransformation [Realization, Tests]

--- HomoGebra Tests/test.cpp
+++ HomoGebra Tests/test.cpp
@@ -9,16 +9,34 @@
 
 namespace HomoGebra
 {
+  constexpr long double epsilon = 1e-6L;
+
+
   namespace Coordinate
   {
+    void check_two_complex(const complex& first, const complex& second, long double abs_error)
+    {
+      ASSERT_NEAR(first.real(), second.real(), abs_error);
+      ASSERT_NEAR(first.imag(), second.imag(), abs_error);
+    }
+
+    void check_two_matrix(const TransformationMatrix& first, const TransformationMatrix& second, long double abs_error)
+    {
+      for (int row = 0; row < std::tuple_size<TransformationMatrix::MatrixColumn>::value; ++row)
+      {
+        for (int column = 0; column < std::tuple_size<TransformationMatrix::MatrixColumn>::value; ++column)
+        {
+          check_two_complex(first[row][column], second[row][column], abs_error);
+        }
+      }
+    }
+
     TEST(TransformationMatrix, Determinant)
     {
       // Create an identity matrix
       TransformationMatrix identity;
 
-      auto inverse = identity.Determinant();
-
-      TransformationMatrix random_matrix(
+      TransformationMatrix matrix(
         complex(-4.924, -2.768),
         complex(0.248, 9.890),
         complex(2.895, 8.700),
@@ -33,15 +51,13 @@ namespace HomoGebra
       );
 
       /*
-      {{-4.924+i*-2.768, 0.248+i*9.890, 2.895+i*8.700},
-      {-5.526+i*-0.244, 5.526+i*-3.827, -7.857+i*1.492},
-      {7.554+i*2.673, -9.136+i*-7.834, 2.573+i*0.384}}
-      determinant is about -292.293 + 116.289*i
+      {{-4.924+i*-2.768, 0.248+i*9.890, 2.895+i*8.700},{-5.526+i*-0.244, 5.526+i*-3.827, -7.857+i*1.492},{7.554+i*2.673, -9.136+i*-7.834, 2.573+i*0.384}}
+      determinant is about complex { -292.293300759 , 116.289449988 }
       */
 
-      auto det = random_matrix.Determinant();
-      ASSERT_NEAR(det.real(), -292.293300759, 0.0000001);
-      ASSERT_NEAR(det.imag(), 116.289449988, 0.0000001);
+      auto det = matrix.Determinant();
+
+      check_two_complex(det, complex { -292.293300759 , 116.289449988 }, epsilon);
     }
 
     TEST(TransformationMatrix, Inverse)
@@ -49,7 +65,44 @@ namespace HomoGebra
       // Create an identity matrix
       TransformationMatrix identity;
 
-      //auto inverse = identity.GetInverse();
+      auto inverse_of_identity = identity.GetInverse();
+
+      ASSERT_TRUE(inverse_of_identity.has_value());
+
+      check_two_matrix(identity, inverse_of_identity.value(), epsilon);
+
+
+      TransformationMatrix matrix(
+        complex(-4.924, -2.768),
+        complex(0.248, 9.890),
+        complex(2.895, 8.700),
+
+        complex(-5.526, -0.244),
+        complex(5.526, -3.827),
+        complex(-7.857, 1.492),
+
+        complex(7.554, 2.673),
+        complex(-9.136, -7.834),
+        complex(2.573, 0.384)
+      );
+
+      auto inverse = matrix.GetInverse();
+
+      auto correct_inverse = TransformationMatrix(
+        complex(0.134816, 0.244013),
+        complex(-0.282593, 0.324476),
+        complex(0.060579, 0.415259),
+        complex(0.137162, 0.0784553),
+        complex(-0.0667268, 0.255604),
+        complex(0.0932031, 0.253248),
+        complex(0.0801455, -0.172217),
+        complex(0.154899, 0.0222525),
+        complex(0.183203, -0.126436)
+      );
+
+      ASSERT_TRUE(inverse.has_value());
+
+      check_two_matrix(correct_inverse, inverse.value(), epsilon);
     }
   }
 
@@ -72,10 +125,4 @@ namespace HomoGebra
   {
 
   }
-}
-
-TEST(TestCaseName, TestName)
-{
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
 }
--- HomoGebra/Coordinate.h
+++ HomoGebra/Coordinate.h
@@ -12,16 +12,39 @@ public:
   using MatrixColumn = std::array<complex, 3>;
   using MatrixContainer = std::array<std::array<complex, 3>, 3>;
 
+  /**
+   * Default Constructor.
+   * 
+   */
   TransformationMatrix();
+
+  /**
+   * Constructor that initializes all elements in matrix
+   *
+   */
   TransformationMatrix(const complex& a00, const complex& a01, const complex& a02,
                        const complex& a10, const complex& a11, const complex& a12,
                        const complex& a20, const complex& a21, const complex& a22);
 
+  /**
+   * Finds inversion of matrix.
+   * 
+   * \return Inverse matrix
+   */
   std::optional<TransformationMatrix> GetInverse() const;
+
+  /**
+   * Calculates determinant
+   *
+   * \return Determinant
+   */
   complex Determinant() const;
 
   const MatrixColumn& operator[](size_t row) const { return matrix_[row]; };
   MatrixColumn& operator[](size_t row) { return matrix_[row]; }
+
+  MatrixContainer::const_iterator begin() const { return matrix_.begin(); }
+  MatrixContainer::const_iterator end() const { return matrix_.end(); }
 private:
   MatrixContainer matrix_;
 };
@@ -46,9 +69,11 @@ struct HomogeneousCoordinate
 
 class Transformation
 {
+public:
+  Transformation(const TransformationMatrix& transformation = TransformationMatrix());
 
 private:
-
+  TransformationMatrix transformation_;
 };
 
 
