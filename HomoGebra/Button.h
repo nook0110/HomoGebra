8911830690380d98ef185a554ff363cc73005e46
nook0110 <nik.blokhtin@gmail.com>
2023-06-27 16:31:26 +0300

    Concepts added

--- HomoGebra/Button.h
+++ HomoGebra/Button.h
@@ -1,21 +1,27 @@
 #pragma once
+#include <cassert>
 #include <utility>
 
-template <class Element, size_t index>
+template <class T, class Arg>
+concept IsSetArgument =
+    requires(T t, Arg&& arg) { t.SetArgument(std::forward<Arg>(arg)); };
+
+template <class Element, size_t index = 0>
 class Wrapper
 {
  public:
   Wrapper() = default;
 
-  template <class T,
-            std::enable_if_t<
-                !std::is_same_v<Wrapper,
-                                std::remove_cv_t<std::remove_reference_t<T>>>,
-                int> = 0>
-  Wrapper(T&& arg);
+  template <class T>
+    requires(!std::same_as<Wrapper<Element, index>, std::remove_cvref_t<T>>)
+  explicit Wrapper(T&& arg);
 
   template <class... Args>
   auto operator()(Args&&... args);
+
+  template <class Arg>
+    requires IsSetArgument<Element, Arg>
+  void SetArgument(Arg&& arg);
 
  private:
   Element element_;
@@ -25,17 +31,22 @@ template <class First, class... Rest>
 class Button : public Wrapper<First, sizeof...(Rest)>, public Button<Rest...>
 {
  public:
-  Button() = default;
+  Button();
 
-  template <class Head, class... Tail,
-            std::enable_if_t<
-                !std::is_same_v<
-                    Button, std::remove_cv_t<std::remove_reference_t<Head>>>,
-                int> = 0>
-  Button(Head&& head, Tail&&... tail);
+  template <class Head, class... Tail>
+    requires(!std::same_as<Button<First, Rest...>, std::remove_cvref_t<Head>>)
+  explicit Button(Head&& head, Tail&&... tail);
 
-  template <class... Arguments>
-  void ConstructObject(Arguments&&... arguments);
+  template <class... Args>
+  void ConstructObject(Args&&... arguments);
+
+  template <class Arg>
+    requires IsSetArgument<Wrapper<First, sizeof...(Rest)>, Arg>
+  void SetArgument(Arg&& arg);
+
+  template <class Arg>
+    requires(!IsSetArgument<Wrapper<First, sizeof...(Rest)>, Arg>)
+  void SetArgument(Arg&& arg);
 };
 
 template <class First>
@@ -44,16 +55,21 @@ class Button<First> : public Wrapper<First, 0>
  public:
   Button();
 
-  template <class... Arguments>
-  void ConstructObject(Arguments&&... arguments);
+  template <class... Args>
+  void ConstructObject(Args&&... arguments);
+
+  template <class Arg>
+    requires IsSetArgument<Wrapper<First, 0>, Arg>
+  void SetArgument(Arg&& arg);
+
+  template <class Arg>
+    requires(!IsSetArgument<Wrapper<First, 0>, Arg>)
+  void SetArgument(Arg&& arg);
 };
 
 template <class Element, size_t index>
-template <class T,
-          std::enable_if_t<
-              !std::is_same_v<Wrapper<Element, index>,
-                              std ::remove_cv_t<std::remove_reference_t<T>>>,
-              int>>
+template <class T>
+  requires(!std::same_as<Wrapper<Element, index>, std::remove_cvref_t<T>>)
 Wrapper<Element, index>::Wrapper(T&& arg) : element_(std::forward<T>(arg))
 {}
 
@@ -61,26 +77,53 @@ template <class Element, size_t index>
 template <class... Args>
 auto Wrapper<Element, index>::operator()(Args&&... args)
 {
-  return element_(args...);
+  return element_(std::forward<Args>(args)...);
+}
+
+template <class Element, size_t index>
+template <class Arg>
+  requires IsSetArgument<Element, Arg>
+void Wrapper<Element, index>::SetArgument(Arg&& arg)
+{
+  element_.SetArgument(std::forward<Arg>(arg));
 }
 
 template <class First, class... Rest>
-template <class Head, class... Tail,
-          std::enable_if_t<
-              !std::is_same_v<Button<First, Rest...>,
-                              std::remove_cv_t<std::remove_reference_t<Head>>>,
-              int>>
+Button<First, Rest...>::Button()
+    : Wrapper<First, sizeof...(Rest)>(), Button<Rest...>()
+{}
+
+template <class First, class... Rest>
+template <class Head, class... Tail>
+  requires(!std::same_as<Button<First, Rest...>, std::remove_cvref_t<Head>>)
 Button<First, Rest...>::Button(Head&& head, Tail&&... tail)
     : Wrapper<First, sizeof...(Rest)>(std::forward<Head>(head)),
       Button<Rest...>(std::forward<Tail>(tail)...)
 {}
 
 template <class First, class... Rest>
-template <class... Arguments>
-void Button<First, Rest...>::ConstructObject(Arguments&&... arguments)
+template <class... Args>
+void Button<First, Rest...>::ConstructObject(Args&&... arguments)
 {
   Button<Rest...>::ConstructObject(
-      arguments..., Wrapper<First, sizeof...(Rest)>::operator()());
+      std::forward<Args>(arguments)...,
+      Wrapper<First, sizeof...(Rest)>::operator()());
+}
+
+template <class First, class... Rest>
+template <class Arg>
+  requires IsSetArgument<Wrapper<First, sizeof...(Rest)>, Arg>
+void Button<First, Rest...>::SetArgument(Arg&& arg)
+{
+  Wrapper<First, 0>::SetArgument(std::forward<Arg>(arg));
+}
+
+template <class First, class... Rest>
+template <class Arg>
+  requires(!IsSetArgument<Wrapper<First, sizeof...(Rest)>, Arg>)
+void Button<First, Rest...>::SetArgument(Arg&& arg)
+{
+  Button<Rest...>::SetArgument(std::forward<Arg>(arg));
 }
 
 template <class First>
@@ -88,8 +131,24 @@ Button<First>::Button() : Wrapper<First, 0>()
 {}
 
 template <class First>
-template <class... Arguments>
-void Button<First>::ConstructObject(Arguments&&... arguments)
+template <class... Args>
+void Button<First>::ConstructObject(Args&&... arguments)
 {
-  Wrapper<First, 0>::operator()(arguments...);
+  Wrapper<First, 0>::operator()(std::forward<Args>(arguments)...);
+}
+
+template <class First>
+template <class Arg>
+  requires IsSetArgument<Wrapper<First, 0>, Arg>
+void Button<First>::SetArgument(Arg&& arg)
+{
+  Wrapper<First, 0>::SetArgument(std::forward<Arg>(arg));
+}
+
+template <class First>
+template <class Arg>
+  requires(!IsSetArgument<Wrapper<First, 0>, Arg>)
+void Button<First>::SetArgument(Arg&& arg)
+{
+  assert(false);
 }
